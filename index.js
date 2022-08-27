dadaeef25594dab1c6e87120d20d75b42a3a6e4f
iamcheeseman <cheeseman085@gmail.com>
2022-08-27 17:22:59 -0400

    Made page titles

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ $(() => {
     $("#socials").load("/socials_buttons.html");
     $("#home-button").prop("innerHTML", "Home"); // Making sure the buttons are all consistent
     $("#home-button").prop("href", "/");
+
+    let title = $(".main-title").prop("innerHTML");
+    document.title = `CheeBlog - ${title}`
 });
